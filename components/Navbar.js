9b09402dd80f1726d51e0e24d018f9e40a48e25b
Aniket Chaudhari <anikethc3@gmail.com>
2021-01-22 03:26:17 +0530

    css fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav style={{ background: '#000' }} className="navbar navbar-expand-lg navbar-dark bg-black">
-      <a style={{ marginLeft: '30px' }} className="navbar-brand" href="/">Aniket Chaudhari</a>
+      <a className="navbar-brand" style={{width: '200px', marginLeft: '30px'}} href="/">Aniket Chaudhari</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
